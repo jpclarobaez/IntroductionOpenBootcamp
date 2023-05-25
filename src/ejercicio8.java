cbcd0e8ae43c80e365b49d5e10ba74671cf0f303
Juan Pablo Claro <jpcb91@gmail.com>
2023-05-25 10:17:59 -0700

    Added exercise 8 of Open-Bootcamp Introducción a la programación

---
+++ src/ejercicio8.java
@@ -0,0 +1,40 @@
+public class ejercicio8 {
+    public static void main(String[] args){
+        System.out.println("Hola");
+        Persona persona1 = new Persona();
+        persona1.setEdad(18);
+        persona1.setTelefono(600700800);
+        persona1.setNombre("Juan");
+
+        System.out.println("La persona creada se llama " + persona1.getNombre() + " y tiene " + persona1.getEdad());
+        System.out.println("Su número de teléfono es " + persona1.getTelefono());
+
+    };
+};
+
+
+class Persona {
+    private int edad;
+    private String nombre;
+    private int telefono;
+
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    public int getEdad(){
+        return this.edad;
+    }
+    public void setTelefono(int telefono){
+        this.telefono = telefono;
+    }
+    public int getTelefono(){
+        return this.telefono;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+}
