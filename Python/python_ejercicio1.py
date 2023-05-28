8b5e9ac6745946c76fcb3d4fddfcc99bb735cda5
Juan Pablo Claro <jpcb91@gmail.com>
2023-05-28 21:44:57 +0200

    AAdded exercises 1 and 2 of Python module

---
+++ Python/python_ejercicio1.py
@@ -0,0 +1,5 @@
+"""Desde la consola de python almacena la cadena “Hola mundo!” en una variable y muéstrala.
+Tienes que subir capturas de pantalla en una carpeta comprimida zip."""
+
+var_string = "Hola mundo!"
+print(var_string)
---
+++ Python/python_ejercicio2.py
@@ -0,0 +1,11 @@
+"""Desde la consola de python almacena la cadena “Hola mundo!” en una variable y muéstrala.
+Tienes que subir capturas de pantalla en una carpeta comprimida zip.
+Modifica la variable del anterior ejercicio en la consola de python y después muestrala por consola para ver la
+modificación de la variable.
+Tienes que subir capturas de pantalla en una carpeta comprimida zip."""
+
+var_string = "Hola mundo!"
+print(var_string)
+
+var_string = var_string.replace('mundo', 'Pepe') + "\n¿Cómo estás?"
+print(var_string)
